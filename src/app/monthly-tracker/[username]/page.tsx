7bb50c62416bf5e0b99dfc13cd11683da70ba7c2
Akshat-1729 <98538556+Akshat-1729@users.noreply.github.com>
2024-09-16 23:59:22 +0530

    added left menu to tracker

--- src/app/monthly-tracker/[username]/page.tsx
+++ src/app/monthly-tracker/[username]/page.tsx
@@ -1,8 +1,20 @@
+import { LeftMenu } from '@/components/leftMenu'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='flex gap-6 pt-6'>
+      <div className="hidden xl:block w-[20%]">
+        <LeftMenu type="profile"/>
+      </div>
+      <div className="w-full lg:w-[70%] xl:w-[80%]">
+        <p>hi</p>
+        
+      </div>
+      
+
+
+    </div>
   )
 }
 export default page
